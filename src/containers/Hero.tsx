f9fa59d62ccd97a8df3897cfc0aecb53cbf64c1d
shiyaam <shiyaamsunderms@gmail.com>
2021-09-03 23:00:05 +0530

    created containers

--- src/containers/Hero.tsx
+++ src/containers/Hero.tsx
@@ -1,10 +1,11 @@
-import { Link as ChakraLink, Flex, Heading, Stack } from '@chakra-ui/react';
+import { Flex, Heading, Stack } from "@chakra-ui/react";
 import {
   Github,
   Instagram,
   Linkedin,
-  Twitter
-} from '@icons-pack/react-simple-icons';
+  Twitter,
+} from "@icons-pack/react-simple-icons";
+import { Link } from "../components";
 
 export const Hero = ({ title }: { title: string }) => (
   <Flex
@@ -14,33 +15,30 @@ export const Hero = ({ title }: { title: string }) => (
     flexDir="column"
   >
     <Stack textAlign="center" spacing={5}>
-      <Heading fontSize={['58px', '64px']} fontWeight="700">
+      <Heading fontSize={["58px", "64px"]} fontWeight="700">
         {title}
       </Heading>
       <Heading>I'm Shiyaam Sunder.</Heading>
       <Heading fontSize="28px">I design and build websites.</Heading>
 
       <Flex alignItems="center" justifyContent="space-around" pt="2rem">
-        <ChakraLink isExternal href="https://github.com/shiyaamsunder">
+        <Link isExternal href="https://github.com/shiyaamsunder">
           <Github size={28} />
-        </ChakraLink>
-        <ChakraLink isExternal href="https://twitter.com/shiyaamsunder">
+        </Link>
+        <Link isExternal href="https://twitter.com/shiyaamsunder">
           <Twitter size={28} />
-        </ChakraLink>
-        <ChakraLink
-          isExternal
-          href="https://www.linkedin.com/in/shiyaamsunderms/"
-        >
+        </Link>
+        <Link isExternal href="https://www.linkedin.com/in/shiyaamsunderms/">
           <Linkedin size={28} />
-        </ChakraLink>
-        <ChakraLink isExternal href="https://www.instagram.com/_surreal_man_/">
+        </Link>
+        <Link isExternal href="https://www.instagram.com/_surreal_man_/">
           <Instagram size={28} />
-        </ChakraLink>
+        </Link>
       </Flex>
     </Stack>
   </Flex>
 );
 
 Hero.defaultProps = {
-  title: 'Hello there 👋'
+  title: "Hello there 👋",
 };
